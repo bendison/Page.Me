0bec7c6a38aa7c5cf2776f883ab53011e29e274b
bendison <discoveriesmc@hotmail.com>
2018-03-11 11:36:39 -0400

    Added methods to setup initial connection, check for contact requests from server, and send responses from teachers

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,13 @@ int param = 0;
 
 String tName = "Jane";
 int joined = 0;
+int request = 0;
+String response = "";
+String nameTest = "";
+
+void setupClientConnection();
+void checkContactRequests();
+void sendTeacherResponse();
 
 void setup() {
     Serial.begin(9600);
@@ -41,16 +48,40 @@ void setup() {
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
 
+    setupClientConnection();
     delay(500);
 }
 
-
 void loop()
 {
-    /*if (Serial.available() > 0) {
-        String test = Serial.readString();*/
+    if (request == 0) {
+        checkContactRequests();
+    }    
+    else {
+        Serial.println("Request in progress.");
+        if (Serial.available() > 0) {
+            String message = Serial.readStringUntil('\n');
+            //if (message.compareTo(":res123") == 0) {
+            if (message.indexOf(":res") == 0) {
+                message.remove(0, 4);
+                response = message;
+                Serial.println("hi there");
+                Serial.println(response);
+                sendTeacherResponse();
+                //send response where we update the name on the server that's getting
+                //requested
+            }
+        }
+        //Read serial for response, then call method to send response to server, 
+        //then set request back to 0 upon delivery confirmation
+    }
+    delay(5000);
+}
+
+void setupClientConnection() {
+    int connected = 0;
+    while(connected == 0) {
         WiFiClient client;
-        
         Serial.printf("\n[Connecting to %s ... ", host);
         if (client.connect(host, 80)) {
             if (joined == 0) {
@@ -76,10 +107,14 @@ void loop()
                     if (client.available()) {
                         String line = client.readStringUntil('\n');
                         if (line.compareTo(tName) == 0) {
-                            //Update joined status
-                            joined = 1;
                             Serial.println("Added to network successfully.");
                         }
+                        else if (line.compareTo("updated") == 0) {
+                            Serial.println("Already existed in the database, but address was different so it has been updated.");
+                        }
+                        //Update joined status                        
+                        joined = 1;    
+                        connected = 1;                    
                     }
                 }
                 client.stop();
@@ -88,6 +123,8 @@ void loop()
             else {
                 Serial.println("connected]");                
                 Serial.println("Already exist in this network.");
+                client.stop();
+                Serial.println("\n[Disconnected]");
             }
         }
         else
@@ -95,6 +132,110 @@ void loop()
             Serial.println("connection failed!]");
             client.stop();
         }
-    //}
-    delay(5000);
+    }
+}
+
+void checkContactRequests() {
+    WiFiClient client;
+    Serial.printf("\n[Connecting to %s ... ", host);
+    if (client.connect(host, 80)) {
+        Serial.println("connected]");
+        Serial.println("Check contact request.");
+        String content = "";
+        client.println("POST /check HTTP/1.1");
+        String h = host;
+        String hostPort = "Host: " + h + ":" + 80;
+        client.println(hostPort);
+        client.println("Cache-Control: no-cache");
+        client.println("Content-Type: application/x-www-form-urlencoded");
+        client.print("Content-Length: ");
+        client.println(content.length());
+        client.println();
+        client.println(content);
+
+        Serial.println("[Response:]");
+        while (client.connected()) {
+            if (client.available()) {
+                String line = client.readStringUntil('\n');
+                Serial.println(line);
+                if (line.compareTo(tName) == 0) {
+                    Serial.println("Request for you.");
+                    request = 1;
+                    nameTest = line;
+                    client.stop();                  
+                    Serial.write(":req");  
+                    //Serial write to arduino, play notifications
+                }      
+                //need other if to check length to see if the teacher "none" so we 
+                //can differentiate from no requests and request for different teacher
+                else {
+                    Serial.println("Request for other teacher.");
+                    //Serial write to arduino, display on screen, no notifications
+                }            
+            }
+        }
+        client.stop();
+        Serial.println("\n[Disconnected]");
+    }
+    else {
+        Serial.println("connection failed!]");
+        //Serial write unable to get requests
+        client.stop();
+    }
+}
+
+void sendTeacherResponse() {
+    int responseCheck = 0;
+    while(responseCheck == 0) {
+        WiFiClient client;
+        Serial.printf("\n[Connecting to %s ... ", host);
+        if (client.connect(host, 80)) {
+            Serial.println("connected]");
+            //response = type of response from teacher (available, away, etc.)
+            String content = "response=";
+            Serial.print("Message Response: ");
+            Serial.println(response);
+            content.concat(response);
+            client.println("POST /response HTTP/1.1");
+            String h = host;
+            String hostPort = "Host: " + h + ":" + 80;
+            client.println(hostPort);
+            client.println("Cache-Control: no-cache");
+            client.println("Content-Type: application/x-www-form-urlencoded");
+            client.print("Content-Length: ");
+            client.println(content.length());
+            client.println();
+            client.println(content);
+
+            Serial.println("[Response:]");
+            while (client.connected()) {
+                if (client.available()) {
+                    String line = client.readStringUntil('\n');
+                    Serial.print("Line: ");
+                    Serial.println(line);
+                    //Convert to integers to remove garbage characters
+                    int a = atoi(line.c_str());
+                    int b = atoi(response.c_str());
+                    //if (line.compareTo(response) == 0) {
+                    if (a == b) {
+                        Serial.println("Response received.");
+                        //reset checks for new requests
+                        response = "";
+                        responseCheck = 1;
+                        request = 0;
+                        client.stop();         
+                    }      
+                    else {
+                        Serial.println("Response not received.");
+                    }            
+                }
+            }
+            client.stop();
+            Serial.println("\n[Disconnected]");
+        }
+        else {
+            Serial.println("connection failed!]");
+            client.stop();
+        }
+    }
 }
